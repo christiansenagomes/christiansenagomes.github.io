329b05bab2d888fac7926b6aa0c18c1f8d5ed984
Christian Sena Gomes <164388478+christiansenagomes@users.noreply.github.com>
2025-10-14 16:48:21 -0300

    Melhorando CSS Cards

--- src/constants.tsx
+++ src/constants.tsx
@@ -8,9 +8,9 @@ export const StudyStorage = {
 }
 
 export const DEFAULT_SUBJECTS = [
-    {"id": 0, "name": "Desenvolvimento de Aplicações Distribuídas e Móveis"},
-    {"id": 1, "name": "Projeto de Software"},
-    {"id": 2, "name": "Estatística e Probabilidade"},
-    {"id": 3, "name": "Gestão da Produção de Software"},
-    {"id": 4, "name": "Trabalho Interdisciplinar: Aplicação Móvel"},
+    {"id": 0, "name": "Desenvolvimento de Aplicações Distribuídas e Móveis", "content": []},
+    {"id": 1, "name": "Projeto de Software", "content": []},
+    {"id": 2, "name": "Estatística e Probabilidade", "content": []},
+    {"id": 3, "name": "Gestão da Produção de Software", "content": []},
+    {"id": 4, "name": "Trabalho Interdisciplinar: Aplicação Móvel", "content": []},
 ];
