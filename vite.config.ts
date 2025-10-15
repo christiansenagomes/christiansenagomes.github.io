311a5e66400c9b72dc585d54249e2f6ebc045967
Christian Sena Gomes <164388478+christiansenagomes@users.noreply.github.com>
2025-10-15 10:14:39 -0300

    Iniciando PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      manifest:{
+        name:"StudyNotes",
+        short_name:"Notes",
+        description:"App de anotações para estudantes",
+        icons:[{
+          src: '/studynotes-192.png',
+          sizes:'192x192',
+          type:'image/png',
+          purpose:'favicon'
+        },
+        {
+          src:'/studynotes-512.png',
+          sizes:'512x512',
+          type:'image/png',
+          purpose:'favicon'
+        },
+      ],
+      display:"standalone",
+      scope:'/',
+      start_url:"/",
+      orientation:'portrait'
+      }
+    }),
+  ],
 })
